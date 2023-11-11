6dbf47cbe9c1c1b2ad16658273b4138a0d868695
purham01 <benjamin.zrakic@fer.hr>
2023-11-11 14:03:37 +0100

    Mali fix

--- Scenes/Merkur.gd
+++ Scenes/Merkur.gd
@@ -7,21 +7,25 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("Hello")
+	timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	level_time_label.text = timer.time_left
+	level_time_label.text = str(timer.time_left)
+	print(timer.time_left)
 
 
 func _on_timer_timeout():
 	player.queue_free()
 
 
-func _on_area_2d_body_entered(body):
+
+
+func _on_sigurna_zona_body_entered(body):
 	timer.stop()
 
 
-func _on_area_2d_body_exited(body):
+func _on_sigurna_zona_body_exited(body):
 	timer.start()
