029ea8a8bc52ddacdd790d62163f61944cd8610d
LovroD77359 <68549602+LovroD77359@users.noreply.github.com>
2023-11-15 17:09:29 +0100

    Earth level, scene tiling, checkpoints fixed
    - made the earth level
    - added movement files for planets
    - fixed checkpoints, they now work if you're using player.gd for player
    - added retracting spikes
    - enabled the use of the Interact layer for tiling with scenes

--- Scenes/Level Assets/Checkpoint.gd
+++ Scenes/Level Assets/Checkpoint.gd
@@ -2,10 +2,10 @@ extends Area2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-signal checkpoint_activated(global_position) 
+signal checkpoint_activated(new_position) 
 
 
 
 func _on_body_entered(body):
 	animated_sprite_2d.play("Active")
-	emit_signal("checkpoint_activated")
+	checkpoint_activated.emit(global_position)
