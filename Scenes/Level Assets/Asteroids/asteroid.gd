668dd812187ac24fd8b6866ce12c2bed2e55b55c
BornPet <borna.petak@fer.hr>
2023-11-27 21:53:19 +0100

    Level + asteroid mechanics

--- Scenes/Level Assets/Asteroids/asteroid.gd
+++ Scenes/Level Assets/Asteroids/asteroid.gd
@@ -37,3 +37,9 @@ func _physics_process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		get_tree().reload_current_scene()
+	if body.is_in_group("TileMap"):
+		get_tree().reload_current_scene()
+		
+		
+		
+		
