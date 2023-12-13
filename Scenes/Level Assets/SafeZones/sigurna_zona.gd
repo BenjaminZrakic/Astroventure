713bb7ba5e2a6a8dda9581519ccf7afedaf91055
BornPet <borna.petak@fer.hr>
2023-12-13 20:25:28 +0100

    merkur boundries

--- Scenes/Level Assets/SafeZones/sigurna_zona.gd
+++ Scenes/Level Assets/SafeZones/sigurna_zona.gd
@@ -13,3 +13,8 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		print("player safe")
+
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		print("player isnt safe")
