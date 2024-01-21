8ca57f0c27bc68784174031e633333302619739e
purham01 <benjamin.zrakic@fer.hr>
2024-01-21 16:31:32 +0100

    Jupiter update

--- Scenes/Levels/JupiterScript.gd
+++ Scenes/Levels/JupiterScript.gd
@@ -3,12 +3,16 @@ extends Node
 @export var moving_spikes : PackedScene
 @export var tilemap : TileMap
 @onready var world = get_node("..")
+@export var disappearing_tiles : PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	addObjects()
+	addSpikes()
+	addDissappearingTiles()
+	tilemap.clear_layer(2)
+	tilemap.clear_layer(3)
 
-func addObjects():
+func addSpikes():
 	var usedCells = tilemap.get_used_cells(2) + tilemap.get_used_cells(3)
 	for cell in usedCells:
 		var cellLayer = 2 if tilemap.get_cell_source_id(2, cell) != -1 else 3
@@ -17,8 +21,7 @@ func addObjects():
 		var place_at = world.to_global(tilemap.map_to_local(cell))
 		if cellSrcId == 3:
 			place_spike(place_at, cellLayer, cellAlt)
-	tilemap.clear_layer(2)
-	tilemap.clear_layer(3)
+	
 
 func place_spike(place_at, cellLayer, cellAlt):
 	var spike_instance = moving_spikes.instantiate()
@@ -42,7 +45,56 @@ func place_spike(place_at, cellLayer, cellAlt):
 		place_at.y -= -10
 	spike_instance.position = place_at
 
+func addDissappearingTiles():
+	var usedCells = tilemap.get_used_cells(2)
+	var cellLayer = 2
+	var cellSrcId
+	var place_at
+	var placed_positions = []
+	var placed_tiles = []
+	for cell in usedCells:
+		place_at = world.to_global(tilemap.map_to_local(cell))
+		if place_at in placed_positions:
+			continue
+		cellSrcId = tilemap.get_cell_source_id(cellLayer, cell)
+		if cellSrcId == 4:
+			var tile_instance = disappearing_tiles.instantiate()
+			place_tile(tile_instance, place_at, cellLayer)
+			placed_positions.append(place_at)
+			placed_tiles.append(tile_instance)
+			check_neighbouring_tiles(cell, usedCells, cellLayer, placed_positions, placed_tiles, tile_instance)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func place_tile(tile_instance, place_at, cellLayer):
+	add_child(tile_instance)
+	place_at.x -=(7+26)
+	place_at.y +=(8+16)
+	tile_instance.position = place_at
+	
+func check_neighbouring_tiles(cell, usedCells, cellLayer, placed_positions, placed_tiles, tile_instance):
+	var place_at
+	var tile_instance_right
+	var tile_instance_left
+	cell.x += 1
+	if cell in usedCells:
+		place_at = world.to_global(tilemap.map_to_local(cell))
+		if place_at not in placed_positions:
+			tile_instance_right = disappearing_tiles.instantiate()
+			place_tile(tile_instance_right, place_at, cellLayer)
+			placed_positions.append(place_at)
+			placed_tiles.append(tile_instance_right)
+			check_neighbouring_tiles(cell, usedCells, cellLayer, placed_positions, placed_tiles, tile_instance_right)
+		else:
+			tile_instance_right = placed_tiles[placed_positions.find(place_at)]
+		tile_instance.tile_right = tile_instance_right
+	cell.x -= 2
+	if cell in usedCells:
+		place_at = world.to_global(tilemap.map_to_local(cell))
+		if place_at not in placed_positions:
+			tile_instance_left = disappearing_tiles.instantiate()
+			place_tile(tile_instance_left, place_at, cellLayer)
+			placed_positions.append(place_at)
+			placed_tiles.append(tile_instance_left)
+			check_neighbouring_tiles(cell, usedCells, cellLayer, placed_positions, placed_tiles, tile_instance_left)
+		else:
+			tile_instance_left = placed_tiles[placed_positions.find(place_at)]
+		tile_instance.tile_left = tile_instance_left
